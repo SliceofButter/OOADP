56abd81b6360a4958bd7a4229dcc83450474fd7f
171107x <171107x@mymail.nyp.edu.sg>
2018-05-23 22:38:48 +0800

    dww

--- app.js
+++ app.js
@@ -7,22 +7,23 @@ const expressValidator = require('express-validator');
 const session = require('express-session');
 
 
-// mongoose.connect('mongodb://localhost/27017');
+mongoose.connect('mongodb://localhost/27017');
 // init app
 const app = express();
 
 //load view engine
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug')
+//bring in models
+let user = require('./models/user');
 
 // body parser middleware
 app.use(bodyParser.urlencoded({extended:false}));
 //parse application
 app.use(bodyParser.json())
  
-
 
 app.get('/', function(req,res){
     res.render('index');
@@ -32,6 +33,11 @@ app.get('/login', function(req,res){
     res.render('login');
 })
 
+// Route Files
+let users = require('./routes/users');
+app.use('/users', users);
+
+//start server
 var port = 3000;
 app.listen(port, function(){
     console.log('App started on port ' + port)
