a7c328d8931b2f0e99f4f776e48668c18fce899a
testing <162793g@mymail.nyp.edu.sg>
2018-06-16 12:35:34 +0800

    2nd push
    attmpting to fix pushing errors

--- routes/productusers.js
+++ routes/productusers.js
@@ -1,11 +1,56 @@
 const express = require('express');
 const router = express.Router();
+const userfinder = require('./users.js');
+
+let Items = require('../models/items');
+//let User = require('../models/user');
+
 
 //Product page
 router.get('/product',function(req,res){
     res.render('product')
     console.log(user.id);
   });  
+
+//Item register
+router.get('/registeritem',function(req,res){
+  res.render('registeritem')
+});
+  
+router.post('/registeritem',function(req,res){
+  const itemname = req.body.itemname;
+  const itemprice = req.body.itemprice;
+  const description = req.body.description;
+  const username = req.body.user;
+
+  
+  
+  req.checkBody('itemname', 'Item Name is required').notEmpty();
+  req.checkBody('itemprice', 'Item Price is required').notEmpty();
+  req.checkBody('description', 'Description is required').notEmpty();
+
+  let errors = req.validationErrors();
+
+  if(errors){
+    res.render('registeritem', {
+      errors:errors
+    });
+  }
+  else{
+    let newUser = new Items({
+      itemname:itemname,
+      itemprice:itemprice,
+      owner: username,
+      description:description
+    });
+  }
+
+  /*User.findById(req.user, function(err, user){
+    res.render('registeritem', { title: 'product', user: user })
+    console.log(user)
+  })*/
+  res.render('registeritem');
+});
 
   
 module.exports = router;
