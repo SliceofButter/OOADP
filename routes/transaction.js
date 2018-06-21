b182f2f37ab19343d8cf5a39c8b7a9552f827736
Max <32918632+SliceofButter@users.noreply.github.com>
2018-06-21 20:53:00 +0800

    Transaction Completed(V1)

--- routes/transaction.js
+++ routes/transaction.js
@@ -3,13 +3,25 @@ const router = express.Router();
 const uuidV4 = require('uuid/v4');
 const userfinder = require('./users.js');
 
+var testID =uuidV4()
+
 let Items = require('../models/items');
+let User = require('../models/user');
 
 router.get('/transaction',function(req,res){
-    res.render('transaction')
+        User.findById(req.user, function(err, user){
+        })
+        var itemlist = Items.find({},function(err, data) {
+            res.render('transaction',{
+            testID,
+            username : req.user,
+            data:data,
+        })
+        }); 
+        
 });
 router.post('/transaction',function(req,res){
-    var testID =uuidV4()
+    
     console.log(testID)
     res.render('transaction')
 })
