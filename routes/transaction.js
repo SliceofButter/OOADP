c05c5f664f9942b4f4644ff99e4f5718fba91222
testing <162793g@mymail.nyp.edu.sg>
2018-08-04 20:43:29 +0800

    fix bug for wallet

--- routes/transaction.js
+++ routes/transaction.js
@@ -60,130 +60,148 @@ router.post('/payment/:id', function (req, res) {
         Banker.findOne({ username: docs.buyer }, function (err, buyer) {
             User.findOne({ username: buyer.username }, function (err, user1) {
                 Banker.findOne({ username: docs.username }, function (err, merch) {
-                    // User.findOne({ username: merch.username }, function (err, user2) {
-                    //     Items.findOne({ _id: docs.id }, function (err, data) {
-                    //         console.log(user1.email);
-                    //         console.log(merch);
-                    //         var transporter = nodemailer.createTransport({ host: 'smtp.gmail.com', port: 465, secure: true, tls: { rejectUnauthorized: false }, auth: { user: 'sghawt@gmail.com', pass: 'NYPIT1704' } });
-                    //         var mailOptions = { from: 'sghawt@gmail.com', to: user1.email, subject: 'Item purchased receipt', text: 'Hello,\n\n' + 'Thanks for purchasing the item ' + docs.itemname + ' at the price of ' + docs.itemprice + '\n\nWe hope to see you again' };
-                    //         var mailOptions2 = { from: 'sghawt@gmail.com', to: user2.email, subject: 'Item purchased receipt', text: 'Hello,\n\n' + docs.itemname + ' has been sold.' + '\n\nWe hope to see you again' };
-                    //         transporter.sendMail(mailOptions2, function (err) {
-                    //             if (err) { console.log(err) }
-                    //             res.send();
-                    //         });
-                    //         transporter.sendMail(mailOptions, function (err) {
-                    //             if (err) { console.log(err) }
-                    //             res.send();
-                    //         });
-                            if (merch == null) {
-                                var itemprice = docs.itemprice;
-                                var newsellwallet = 0 + itemprice;
-                                newBalance = new Banker({
-                                    username: docs.username,
-                                    amount: newsellwallet
-                                })
-                                newBalance.save(function (err) {
-                                    if (err) {
-                                        console.log(err);
-                                        return;
-                                    } else {
-                                        res.send();
-                                    }
-                                })
-                                newbuywallet = buyer.amount - itemprice
-                                Banker.findOneAndUpdate({ username: docs.buyer }, { $set: { amount: newbuywallet } }, function (err) {
-                                    if (err) {
-                                        console.log(err);
-                                        return;
-                                    }
-                                    else { res.send(); }
-                                });
-                                WishlistItem.findOneAndRemove({ id: docs.id }, function (err) {
-                                    if (err) {
-                                        console.log(err);
-                                        return;
-                                    }
-                                    else {
-                                        res.send();
-                                    }
-                                })
-                                Items.findByIdAndRemove({ _id: docs.id }, function (err) {
-                                    if (err) {
-                                        console.log(err);
-                                        return;
-                                    } else {
-                                        res.send();
-                                    }
-                                })
-                                Transacs.findOneAndUpdate({ uniqueID: req.params.id }, { $set: { status: 'Paid' } }, function (err) {
-                                    if (err) {
-                                        console.log(err);
-                                        return;
-                                    }
-                                    else { res.send(); }
-                                });
-                                alert('Item has been bought!');
-                                res.redirect('/');
+                    if (merch == null) {
+                        var itemprice = docs.itemprice;
+                        var newsellwallet = 0 + itemprice;
+                        var newusername = docs.username;
+                        newBalance = new Banker({
+                            username: docs.username,
+                            amount: newsellwallet
+                        })
+                        newBalance.save(function (err) {
+                            if (err) {
+                                console.log(err);
+                                return;
                             } else {
+                                res.send();
+                            }
+                        })
+                        newbuywallet = buyer.amount - itemprice
+                        Banker.findOneAndUpdate({ username: docs.buyer }, { $set: { amount: newbuywallet } }, function (err) {
+                            if (err) {
+                                console.log(err);
+                                return;
+                            }
+                            else { res.send(); }
+                        });
+                        WishlistItem.findOneAndRemove({ id: docs.id }, function (err) {
+                            if (err) {
+                                console.log(err);
+                                return;
+                            }
+                            else {
+                                res.send();
+                            }
+                        })
+                        Items.findByIdAndRemove({ _id: docs.id }, function (err) {
+                            if (err) {
+                                console.log(err);
+                                return;
+                            } else {
+                                res.send();
+                            }
+                        })
+                        Transacs.findOneAndUpdate({ uniqueID: req.params.id }, { $set: { status: 'Paid' } }, function (err) {
+                            if (err) {
+                                console.log(err);
+                                return;
+                            }
+                            else { res.send(); }
+                        });
+                        User.findOne({ username: newusername }, function (err, user2) {
+                            Items.findOne({ _id: docs.id }, function (err, data) {
+                                console.log(user1.email);
+                                console.log(merch);
+                                var transporter = nodemailer.createTransport({ host: 'smtp.gmail.com', port: 465, secure: true, tls: { rejectUnauthorized: false }, auth: { user: 'sghawt@gmail.com', pass: 'NYPIT1704' } });
+                                var mailOptions = { from: 'sghawt@gmail.com', to: user1.email, subject: 'Item purchased receipt', text: 'Hello,\n\n' + 'Thanks for purchasing the item ' + docs.itemname + ' at the price of ' + docs.itemprice + '\n\nWe hope to see you again' };
+                                var mailOptions2 = { from: 'sghawt@gmail.com', to: user2.email, subject: 'Item purchased receipt', text: 'Hello,\n\n' + docs.itemname + ' has been sold.' + '\n\nWe hope to see you again' };
+                                transporter.sendMail(mailOptions2, function (err) {
+                                    if (err) { console.log(err) }
+                                    res.send();
+                                });
+                                transporter.sendMail(mailOptions, function (err) {
+                                    if (err) { console.log(err) }
+                                    res.send();
+                                });
+                            })
+                        })
+                        alert('Item has been bought!');
+                        res.redirect('/');
+                    } else {
 
-                                var buywallet = buyer.amount;
-                                var itemprice = docs.itemprice;
-                                var fee = 3;
-                                var sellwallet = merch.amount;
-                                if (docs.deliverymethod == "Delivery") {
-                                    var newbuywallet = buywallet - itemprice - fee;
-                                }
-                                else {
-                                    var newbuywallet = buywallet - itemprice;
-                                }
-                                var newsellwallet = sellwallet + itemprice;
-                                Banker.findOneAndUpdate({ username: docs.username }, { $set: { amount: newsellwallet } }, function (err) {
-                                    if (err) {
-                                        console.log(err);
-                                        return;
-                                    }
-                                    else { res.send(); }
+                        var buywallet = buyer.amount;
+                        var itemprice = docs.itemprice;
+                        var fee = 3;
+                        var sellwallet = merch.amount;
+                        if (docs.deliverymethod == "Delivery") {
+                            var newbuywallet = buywallet - itemprice - fee;
+                        }
+                        else {
+                            var newbuywallet = buywallet - itemprice;
+                        }
+                        var newsellwallet = sellwallet + itemprice;
+                        Banker.findOneAndUpdate({ username: docs.username }, { $set: { amount: newsellwallet } }, function (err) {
+                            if (err) {
+                                console.log(err);
+                                return;
+                            }
+                            else { res.send(); }
+                        });
+                        Banker.findOneAndUpdate({ username: docs.buyer }, { $set: { amount: newbuywallet } }, function (err) {
+                            if (err) {
+                                console.log(err);
+                                return;
+                            }
+                            else { res.send(); }
+                        });
+                        WishlistItem.findOneAndRemove({ id: docs.id }, function (err) {
+                            if (err) {
+                                console.log(err);
+                                return;
+                            }
+                            else {
+                                res.send();
+                            }
+                        })
+                        Items.findByIdAndRemove({ _id: docs.id }, function (err) {
+                            if (err) {
+                                console.log(err);
+                                return;
+                            } else {
+                                res.send();
+                            }
+                        })
+                        Transacs.findOneAndUpdate({ uniqueID: req.params.id }, { $set: { status: 'Paid' } }, function (err) {
+                            if (err) {
+                                console.log(err);
+                                return;
+                            }
+                            else { res.send(); }
+                        });
+                        User.findOne({ username: merch.username }, function (err, user2) {
+                            Items.findOne({ _id: docs.id }, function (err, data) {
+                                console.log(user1.email);
+                                console.log(merch);
+                                var transporter = nodemailer.createTransport({ host: 'smtp.gmail.com', port: 465, secure: true, tls: { rejectUnauthorized: false }, auth: { user: 'sghawt@gmail.com', pass: 'NYPIT1704' } });
+                                var mailOptions = { from: 'sghawt@gmail.com', to: user1.email, subject: 'Item purchased receipt', text: 'Hello,\n\n' + 'Thanks for purchasing the item ' + docs.itemname + ' at the price of ' + docs.itemprice + '\n\nWe hope to see you again' };
+                                var mailOptions2 = { from: 'sghawt@gmail.com', to: user2.email, subject: 'Item purchased receipt', text: 'Hello,\n\n' + docs.itemname + ' has been sold.' + '\n\nWe hope to see you again' };
+                                transporter.sendMail(mailOptions2, function (err) {
+                                    if (err) { console.log(err) }
+                                    res.send();
                                 });
-                                Banker.findOneAndUpdate({ username: docs.buyer }, { $set: { amount: newbuywallet } }, function (err) {
-                                    if (err) {
-                                        console.log(err);
-                                        return;
-                                    }
-                                    else { res.send(); }
-                                });
-                                WishlistItem.findOneAndRemove({ id: docs.id }, function (err) {
-                                    if (err) {
-                                        console.log(err);
-                                        return;
-                                    }
-                                    else {
-                                        res.send();
-                                    }
-                                })
-                                Items.findByIdAndRemove({ _id: docs.id }, function (err) {
-                                    if (err) {
-                                        console.log(err);
-                                        return;
-                                    } else {
-                                        res.send();
-                                    }
-                                })
-                                Transacs.findOneAndUpdate({ uniqueID: req.params.id }, { $set: { status: 'Paid' } }, function (err) {
-                                    if (err) {
-                                        console.log(err);
-                                        return;
-                                    }
-                                    else { res.send(); }
+                                transporter.sendMail(mailOptions, function (err) {
+                                    if (err) { console.log(err) }
+                                    res.send();
                                 });
                                 alert('Item has been bought!');
                                 res.redirect('/');
                             }
-                    //     })
-                    // })
+                        )}
+                        )}
                 })
             })
         })
-    });
+    })
 })
 
 module.exports = router;
