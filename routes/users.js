ac459866d4527830761cf856847b41dc752494d5
Sol <32918650+171107x@users.noreply.github.com>
2018-08-10 19:22:42 +0800

    ladi dadi

--- routes/users.js
+++ routes/users.js
@@ -355,7 +355,7 @@ router.get('/profile/:username', ensureAuthenticated, function (req, res, next) 
             res.render('profile', {
               current: current.username,
               bio: user.bio,
-              pic: user.dp,
+              pic: current.dp,
               data: data,
               // follow : follow.following,
               wallet: bank,
